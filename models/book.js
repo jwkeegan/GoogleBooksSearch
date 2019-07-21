4baf3330295fc8063e565c3e03adda085d5a86cc
Joe Keegan <jwkeeg@comcast.net>
2019-07-21 14:43:34 -0500

    fixed typo in Book model

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     title: { type: String, required: true },
     authors: { type: String, required: true },
-    descriptino: String,
+    description: String,
     image: String,
     link: { type: String, required: true },
     date: { type: Date, default: Date.now }
