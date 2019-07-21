bb7f184e2a44a6f5eeef2bfc6374ea99299b987c
Joe Keegan <jwkeeg@comcast.net>
2019-07-21 18:20:38 -0500

    Nav.js Navbar

---
+++ google-books-search/src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a className="navbar-brand" href="/">
+                Google Books
+            </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Search</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/saved">Saved</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
